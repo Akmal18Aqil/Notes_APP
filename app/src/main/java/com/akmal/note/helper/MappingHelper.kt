01da3161d92461c598f9ec61740d78841a488cde
Akmal <akmalaqilwahyu@gmail.com>
2023-09-28 20:40:24 +0700

    Clear dengan database lokal

---
+++ app/src/main/java/com/akmal/note/helper/MappingHelper.kt
@@ -0,0 +1,22 @@
+package com.akmal.note.helper
+
+import android.database.Cursor
+import com.akmal.note.db.DatabaseContract
+import com.akmal.note.entity.Note
+
+object MappingHelper {
+    fun MapCursorToArrayList(notesCursor: Cursor?): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+}
